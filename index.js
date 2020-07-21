0d34d7ed3041e9d38869f2a4a22dcf261470b96e
sulenchy <sulaimanabiodun172@gmail.com>
2020-07-21 18:01:00 +0100

    completed getting professors info for lancaster

--- index.js
+++ index.js
@@ -2,58 +2,97 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var CircularJSON = require('circular-json');
+var app = express();
 
-app.get('/scrape', function(req, res){
-    const testUrl = 'https://www.lancaster.ac.uk/search/?collection=lancaster-meta&query=computer&tab=people&start_rank=11';
+app.get('/scrape', function (req, res) {
 
-    url = 'http://www.imdb.com/title/tt1229340/';
-    
-    request(testUrl, function(error, response, html){
-        console.log('===> ', html);
+  //get professor's info in lancaster
+  /**
+  var lancasterUrl = 'https://www.lancaster.ac.uk/search/?collection=lancaster-meta&query=computer&tab=people&start_rank=11';
 
-        if(!error){
-            var $ = cheerio.load(html);
-    
-            var json = [];
-        
-            $('.staff-details').filter(function(){
-                var data = $(this);
-                fullName = data.children().first().text();
-                title = data.children('job-title').text();
-                specialization = data.children().last().children().text();
-                phoneNumber = data.children().last().children().text();
+  request(lancasterUrl, function (error, response, html) {
 
-        
-                json.push({ fullName, title, specialization, phoneNumber });
+    if (!error) {
+      var $ = cheerio.load(html);
 
-            })
-        
-            $('.star-box-giga-star').filter(function(){
-                var data = $(this);
-                rating = data.text();
-        
-                json.rating = rating;
-            })
-        }
-    
+      var json = [];
+
+      $('.fb-result__info').filter(function () {
+        var data = $(this);
+        var fullName = data.children().first().children('.staff-details').children().first().text();
+        var title = data.children().first().children('.staff-details').children().last().text();
+        var phoneNumber = data.children().last().children('.staff-contact').children().first().text();
+        var department = data.children().last().children('.staff-contact').children().last().text();
+        var readMoreLink = '';
+        var university= 'lancaster';
+
+        json.push({ fullName, title, phoneNumber, department, readMoreLink, university });
+      })
+    }
+
     // To write to the system we will use the built in 'fs' library.
     // In this example we will pass 3 parameters to the writeFile function
     // Parameter 1 :  output.json - this is what the created filename will be called
     // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
     // Parameter 3 :  callback function - a callback function to let us know the status of our function
-    
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-    
-        console.log('File successfully written! - Check your project directory for the output.json file');
-    
+
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+
+      console.log('File successfully written! - Check your project directory for the output.json file');
+
     })
-    
+
     // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
     res.send('Check your console!')
-    
-        }) ;
+
+  });
+
+  */
+
+  //get professor's info incoventry
+  var coventryUrl = 'https://www.coventry.ac.uk/life-on-campus/staff-directory/?filters=1187';
+  
+  request(coventryUrl, function (error, response, html) {
+
+    if (!error) {
+      var $ = cheerio.load(html);
+
+      var json = [];
+
+      $('.container').filter(function () {
+        var data = $(this);
+        var fullName = data.children('.row').first().children('.mtm').content();
+
+        console.log('fullname ===> ', fullName);
+        // var title = data.children().first().children('.staff-details').children().last().text();
+        // var phoneNumber = data.children().last().children('.staff-contact').children().first().text();
+        // var department = data.children().last().children('.staff-contact').children().last().text();
+        // var readMoreLink = '';
+        // var university= 'lancaster';
+
+        json.push({ fullName });
+      })
+    }
+
+    // To write to the system we will use the built in 'fs' library.
+    // In this example we will pass 3 parameters to the writeFile function
+    // Parameter 1 :  output.json - this is what the created filename will be called
+    // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+    // Parameter 3 :  callback function - a callback function to let us know the status of our function
+
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+
+      console.log('File successfully written! - Check your project directory for the output.json file');
+
     })
+
+    // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+    res.send('Check your console!')
+
+  });
+  
+})
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
